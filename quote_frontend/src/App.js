962ec5c3562dc7bf927f533b2e3601497a48bbac
Dustin Bingham <djbingham88@gmail.com>
2021-03-12 09:50:46 -0600

    Added generic front and backend

---
+++ quote_frontend/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  //how to handle grabbing json data on the front end
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+
+  //this is whats on the screen
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {!data ? "Loading..." : data}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
