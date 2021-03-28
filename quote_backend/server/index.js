3734da4b0555e3b02db91bcc2fd7c2fcb6c6021e
Dustin Bingham <djbingham88@gmail.com>
2021-03-28 12:27:43 -0500

    added mysql functionality and one call to the sales table

--- quote_backend/server/index.js
+++ quote_backend/server/index.js
@@ -1,12 +1,42 @@
 const express = require("express");
+var mysql = require('mysql');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//createing connection to mysql server
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'mysql'
+})
+
+connection.connect()
+
+//make all database querys below
+
+/**
+ * selects all from sales table 
+ * sends result to http://localhost:3001/sales
+ */
+connection.query('SELECT * FROM sales', function(err, rows, fields){
+    if(err) throw err
+    //Sales endpoint
+    app.get("/sales", (req, res) => {
+    res.json(rows)
+    })//end get   
+})//end query
+
+
+connection.end()
+//end of database querys
+
+
+
 //Generic endpoint
 app.get("/", (req, res) => {
-    res.send("Express App")
+    res.send("Hello World") 
 })
 
 //end point named api
