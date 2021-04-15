17cf5f0c3977a621bca890ff4a189e6830b0dd5f
ChristianJerret <z1865959@students.niu.edu>
2021-04-15 15:29:29 -0500

    added retrieve quote php

---
+++ quote_backend/server/retrieveQuoteOnID.php
@@ -0,0 +1,20 @@
+D = $_POST['qid'];
+
+    $host = "localhost";
+    $user = "root";
+    $dbname = "mysql";
+
+    $dsn = "mysql:dbname=$dbname;host=$host";
+    $pdo = new PDO($dsn, $user);
+
+    $res = $pdo->prepare("SELECT * FROM newquote WHERE qID ='$ID';");
+    $res->execute();
+
+    $data = $res->fetch(PDO::FETCH_ASSOC);
+
+    echo json_encond($data);
+
+    $res = NULL;
+    $pdo = NULL;
+
+?>
