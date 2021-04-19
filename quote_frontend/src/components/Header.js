236b08168145fb46dc5f3ae14ed51c6953379c00
Edward Owusu <eaowusu18@gmail.com>
2021-04-19 15:34:55 -0500

    Fixed Header

--- quote_frontend/src/components/Header.js
+++ quote_frontend/src/components/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 export default function Header() {
     return (
-        <head>
-            <header>
-                
-            </header>
-        </head>
+        <div>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography variant="h6">
+                            4B Quotes: Management System
+                        </Typography>
+                        <Button href="http://localhost:3000/salesLand">
+                            Sales
+                        </Button>
+                        <Button href="http://localhost:3000/adminPage">
+                            Admin
+                        </Button>
+                        <Button href="http://localhost:3000/hqLand">
+                            HQ
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
     )
 }
